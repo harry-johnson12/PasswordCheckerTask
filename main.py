cb4b3394a643c9220f1e00fa643ca98f5f2da571
Harry Johnson <hjohnson@student.saintaug.nsw.edu.au>
2025-05-16 19:24:40 +1000

    implemented a class for creating labels, added a title label and password entry widget

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import customtkinter as ctk
 
-WIDTH = 800
-HEIGHT = 600
+#IDEAS(GUI): about, settings, help, show password, by harry j, customise for light/dark mode
+#IDEAS(SECURITY): password strength/breach checker, password generator, password manager
+#size parameter could fix the cut off text issue
 
-
+WIDTH = 600
+HEIGHT = 400
+FONT = "Helvetica Neue Medium"
 
 app = ctk.CTk()
 app.title("PassPy")
@@ -12,7 +15,32 @@ app._set_appearance_mode("light") # Modes: "system" (default), "dark", "light"
 #app.iconbitmap("assets/icon.ico")
 # app.resizable(False, False) 
 
-label = ctk.CTkLabel(app, text="PassPy", font=("Arial Rounded MT Bold", 30)) 
-label.place(relx=0.5, rely=0.1, anchor="center")
+class Frame:
+    pass
+
+class Button:
+    pass
+
+class Label:
+    def __init__(self, window, text, x, y, font, size, color):
+        self.window = window
+        self.text = text
+        self.x = x
+        self.y = y
+        self.font = font
+        self.size = size
+        self.color = color
+        self.label = ctk.CTkLabel(self.window, text=self.text, font=(self.font, self.size), text_color=self.color)
+
+    def place(self):
+        self.label.place(relx=self.x, rely=self.y, anchor="sw")
+    
+titleLbl = Label(app, "PassPy", 0.03, 0.12, FONT, 28, "black")
+enterPassLbl = Label(app, "Enter Password", 0.03, 0.24, FONT, 20, "black",)
+passwordEntry = ctk.CTkEntry(app, corner_radius=3, placeholder_text="Password", width=200, show="*", font=(FONT, 15))
+
+titleLbl.place()
+enterPassLbl.place()
+passwordEntry.place(relx=0.3, rely=0.24, anchor="sw")
 
 app.mainloop()
