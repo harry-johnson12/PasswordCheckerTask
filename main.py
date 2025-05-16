2d3efd24604e45f9aa8afd7f57657e251d3c993a
Harry Johnson <hjohnson@student.saintaug.nsw.edu.au>
2025-05-16 18:20:36 +1000

    basic label working

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import customtkinter as ctk
 WIDTH = 800
 HEIGHT = 600
 
+
+
 app = ctk.CTk()
 app.title("PassPy")
 app.geometry(f"{WIDTH}x{HEIGHT}")
-app.resizable(False, False) 
+app._set_appearance_mode("light") # Modes: "system" (default), "dark", "light"
 #app.iconbitmap("assets/icon.ico")
-#app.set_appearance_mode("dark")
+# app.resizable(False, False) 
+
+label = ctk.CTkLabel(app, text="PassPy", font=("Arial Rounded MT Bold", 30)) 
+label.place(relx=0.5, rely=0.1, anchor="center")
 
 app.mainloop()
