3245e1a95bfbda0f2b4b77e921182bff25e37ddd
Harry Johnson <hjohnson@student.saintaug.nsw.edu.au>
2025-05-20 20:32:48 +1000

    created the password checker class and implemented the ability of the enter button to update the stored password inside this class

--- main.py
+++ main.py
@@ -17,23 +17,31 @@ app._set_appearance_mode("light") # Modes: "system" (default), "dark", "light"
 app.resizable(False, False) 
 #app.iconbitmap("assets/icon.ico")
 
-class Password:
-    def __init__(self, password):
-        pass
-    
-# Create the main window
-passwordEntry = ctk.CTkEntry(app, corner_radius=CORNER_RADIUS, placeholder_text="Password", width=200, show="*", font=(FONT, 15))
-titleLbl = ctk.CTkLabel(app, text="PassPy", font=(FONT, 28), text_color="black")
-enterPassLbl = ctk.CTkLabel(app, text="Enter Password", font=(FONT, 20), text_color="black")
-creditLbl = ctk.CTkLabel(app, text="A password strength checker by Harry Johnson.", font=(FONT, 12), text_color="dark grey")
-enterButton = ctk.CTkButton(app, text="Enter", font=(FONT, 12), width=60, height=30, text_color="black", fg_color="light grey", hover_color="dark grey", corner_radius=CORNER_RADIUS)
-showButton = ctk.CTkButton(app, text="Show", font=(FONT, 12), width=60, height=30, text_color="black", fg_color="light grey", hover_color="dark grey", corner_radius=CORNER_RADIUS)
 
-titleLbl.place(relx=0.03, rely=0.12, anchor="sw")
-enterPassLbl.place(relx=0.03, rely=0.24, anchor="sw")
-creditLbl.place(relx=0.2, rely=0.14, anchor="sw")
-enterButton.place(relx=0.64, rely=0.24, anchor="sw")
-showButton.place(relx=0.75, rely=0.24, anchor="sw")
-passwordEntry.place(relx=0.29, rely=0.24, anchor="sw")
+password_entry = ctk.CTkEntry(app, corner_radius=CORNER_RADIUS, placeholder_text="Password", width=200, show="*", font=(FONT, 15))
+
+class Password_checker:
+    def __init__(self):
+        self.password = ""
+    
+    def update_password(self):
+        self.password = password_entry.get()
+        print(self.password)
+
+password_checker = Password_checker()
+
+enter_button = ctk.CTkButton(app, text="Enter", command=password_checker.update_password, font=(FONT, 12), width=60, height=30, text_color="black", fg_color="light grey", hover_color="dark grey", corner_radius=CORNER_RADIUS)
+show_button = ctk.CTkButton(app, text="Show", font=(FONT, 12), width=60, height=30, text_color="black", fg_color="light grey", hover_color="dark grey", corner_radius=CORNER_RADIUS)
+
+title_lbl = ctk.CTkLabel(app, text="PassPy", font=(FONT, 28), text_color="black")
+enter_pass_lbl = ctk.CTkLabel(app, text="Enter Password", font=(FONT, 20), text_color="black")
+credit_lbl = ctk.CTkLabel(app, text="A password strength checker by Harry Johnson.", font=(FONT, 12), text_color="dark grey")
+
+password_entry.place(relx=0.29, rely=0.24, anchor="sw")
+enter_button.place(relx=0.64, rely=0.24, anchor="sw")
+show_button.place(relx=0.75, rely=0.24, anchor="sw")
+title_lbl.place(relx=0.03, rely=0.12, anchor="sw")
+enter_pass_lbl.place(relx=0.03, rely=0.24, anchor="sw")
+credit_lbl.place(relx=0.2, rely=0.14, anchor="sw")
 
 app.mainloop()
