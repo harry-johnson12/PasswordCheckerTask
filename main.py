8749095eb375d746b49a791c5cb1baa11907512c
Harry Johnson <hjohnson@student.saintaug.nsw.edu.au>
2025-05-20 19:41:38 +1000

    removed unnesecary label and button classes, now just using the inbuilt customtk classes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import customtkinter as ctk
 
 #IDEAS(GUI): about, settings, help, by harry j, customise for light/dark mode
-#IDEAS(SECURITY): password strength/breach checker - common password list, api
-#password generator, password manager
+#IDEAS(SECURITY): password strength/breach checker - length check, character diversity, common password list, api breaches, overall score out of 100
+#could also have a password generator, password manager, potentially AI password improver
 #size parameter could fix the cut off text issue, eventually make it resizable
 
 WIDTH = 600
@@ -14,43 +14,26 @@ app = ctk.CTk()
 app.title("PassPy")
 app.geometry(f"{WIDTH}x{HEIGHT}")
 app._set_appearance_mode("light") # Modes: "system" (default), "dark", "light"
+app.resizable(False, False) 
 #app.iconbitmap("assets/icon.ico")
-# app.resizable(False, False) 
 
-class Frame:
-    pass
-
-class Button:
-    def __init__(self, window, text, x, y, font, font_size, width, height, text_color, bg_color="light grey", corner_radius=CORNER_RADIUS):
-        self.x = x
-        self.y = y
-        self.button = ctk.CTkButton(window, text=text, font=(font, font_size), width=width, height=height, text_color=text_color, fg_color=bg_color, corner_radius=corner_radius)
-
-    def place(self):
-        self.button.place(relx=self.x, rely=self.y, anchor="sw")
-
-class Label:
-    def __init__(self, window, text, x, y, font, font_size, colour):
-        self.x = x
-        self.y = y
-        self.label = ctk.CTkLabel(window, text=text, font=(font, font_size), text_color=colour)
-
-    def place(self):
-        self.label.place(relx=self.x, rely=self.y, anchor="sw")
-
-titleLbl = Label(app, "PassPy", 0.03, 0.12, FONT, 28, "black")
-enterPassLbl = Label(app, "Enter Password", 0.03, 0.24, FONT, 20, "black",)
-creditLbl = Label(app, "A password strength checker by Harry Johnson.", 0.21, 0.14, FONT, 12, "dark grey")
-
-enterButton = Button(app, "Enter", 0.64, 0.24, FONT, 15, 60, 30, "black")
-showButton = Button(app, "Show", 0.75, 0.24, FONT, 15, 60, 30, "black")
+class Password:
+    def __init__(self, password):
+        pass
+    
+# Create the main window
 passwordEntry = ctk.CTkEntry(app, corner_radius=CORNER_RADIUS, placeholder_text="Password", width=200, show="*", font=(FONT, 15))
+titleLbl = ctk.CTkLabel(app, text="PassPy", font=(FONT, 28), text_color="black")
+enterPassLbl = ctk.CTkLabel(app, text="Enter Password", font=(FONT, 20), text_color="black")
+creditLbl = ctk.CTkLabel(app, text="A password strength checker by Harry Johnson.", font=(FONT, 12), text_color="dark grey")
+enterButton = ctk.CTkButton(app, text="Enter", font=(FONT, 12), width=60, height=30, text_color="black", fg_color="light grey", hover_color="dark grey", corner_radius=CORNER_RADIUS)
+showButton = ctk.CTkButton(app, text="Show", font=(FONT, 12), width=60, height=30, text_color="black", fg_color="light grey", hover_color="dark grey", corner_radius=CORNER_RADIUS)
 
-titleLbl.place()
-enterPassLbl.place()
-creditLbl.place()
-enterButton.place()
-showButton.place()
+titleLbl.place(relx=0.03, rely=0.12, anchor="sw")
+enterPassLbl.place(relx=0.03, rely=0.24, anchor="sw")
+creditLbl.place(relx=0.2, rely=0.14, anchor="sw")
+enterButton.place(relx=0.64, rely=0.24, anchor="sw")
+showButton.place(relx=0.75, rely=0.24, anchor="sw")
 passwordEntry.place(relx=0.29, rely=0.24, anchor="sw")
 
 app.mainloop()
