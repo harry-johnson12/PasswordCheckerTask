2b15eb70565c537a17b6960fa3ec7c28febcfe37
Harry Johnson <hjohnson@student.saintaug.nsw.edu.au>
2025-05-21 17:39:59 +1000

    encased all of the GUI elements into one App class

--- main.py
+++ main.py
@@ -1,47 +1,49 @@
-import customtkinter as ctk
 
-#IDEAS(GUI): about, settings, help, by harry j, customise for light/dark mode
+#IDEAS(GUI): settings, help, customise for light/dark mode
+
 #IDEAS(SECURITY): password strength/breach checker - length check, character diversity, common password list, api breaches, overall score out of 100
-#could also have a password generator, password manager, potentially AI password improver
-#size parameter could fix the cut off text issue, eventually make it resizable
+
+#OTHER: also have a password generator, password manager, potentially AI password improver, size parameter could fix the cut off text issue, eventually make it resizable
+
+import customtkinter as ctk
 
 WIDTH = 600
 HEIGHT = 400
 FONT = "Helvetica Neue Medium"
 CORNER_RADIUS = 3
 
-app = ctk.CTk()
-app.title("PassPy")
-app.geometry(f"{WIDTH}x{HEIGHT}")
-app._set_appearance_mode("light") # Modes: "system" (default), "dark", "light"
-app.resizable(False, False) 
-#app.iconbitmap("assets/icon.ico")
+class App(ctk.CTk):
+    def __init__(self, password_checker):
+        super().__init__()
+        self.password_checker = password_checker
+        self.title("PassPy")
+        self.geometry(f"{WIDTH}x{HEIGHT}")
+        self._set_appearance_mode("light") # Modes: "system" (default), "dark", "light"
+        self.resizable(False, False) 
+        #self.iconbitmap("assets/icon.ico")
+        
+        self.password_entry = ctk.CTkEntry(self, corner_radius=CORNER_RADIUS, placeholder_text="Password", width=200, show="*", font=(FONT, 15))
+        self.show_button = ctk.CTkButton(self, text="Show", font=(FONT, 12), width=60, height=30, text_color="black", fg_color="light grey", hover_color="dark grey", corner_radius=CORNER_RADIUS)
+        self.enter_button = ctk.CTkButton(self, text="Enter", command=password_checker.update_password, font=(FONT, 12), width=60, height=30, text_color="black", fg_color="light grey", hover_color="dark grey", corner_radius=CORNER_RADIUS)
+        self.title_lbl = ctk.CTkLabel(self, text="PassPy", font=(FONT, 28), text_color="black")
+        self.enter_pass_lbl = ctk.CTkLabel(self, text="Enter Password", font=(FONT, 20), text_color="black")
+        self.credit_lbl = ctk.CTkLabel(self, text="A password strength checker by Harry Johnson.", font=(FONT, 12), text_color="dark grey")
 
-
-password_entry = ctk.CTkEntry(app, corner_radius=CORNER_RADIUS, placeholder_text="Password", width=200, show="*", font=(FONT, 15))
+        self.password_entry.place(relx=0.29, rely=0.24, anchor="sw")
+        self.enter_button.place(relx=0.64, rely=0.24, anchor="sw")
+        self.show_button.place(relx=0.75, rely=0.24, anchor="sw")
+        self.title_lbl.place(relx=0.03, rely=0.12, anchor="sw")
+        self.enter_pass_lbl.place(relx=0.03, rely=0.24, anchor="sw")
+        self.credit_lbl.place(relx=0.2, rely=0.14, anchor="sw")
 
 class Password_checker:
     def __init__(self):
-        self.password = ""
+        self.password = None
     
     def update_password(self):
-        self.password = password_entry.get()
+        self.password = app.password_entry.get()
         print(self.password)
 
 password_checker = Password_checker()
-
-enter_button = ctk.CTkButton(app, text="Enter", command=password_checker.update_password, font=(FONT, 12), width=60, height=30, text_color="black", fg_color="light grey", hover_color="dark grey", corner_radius=CORNER_RADIUS)
-show_button = ctk.CTkButton(app, text="Show", font=(FONT, 12), width=60, height=30, text_color="black", fg_color="light grey", hover_color="dark grey", corner_radius=CORNER_RADIUS)
-
-title_lbl = ctk.CTkLabel(app, text="PassPy", font=(FONT, 28), text_color="black")
-enter_pass_lbl = ctk.CTkLabel(app, text="Enter Password", font=(FONT, 20), text_color="black")
-credit_lbl = ctk.CTkLabel(app, text="A password strength checker by Harry Johnson.", font=(FONT, 12), text_color="dark grey")
-
-password_entry.place(relx=0.29, rely=0.24, anchor="sw")
-enter_button.place(relx=0.64, rely=0.24, anchor="sw")
-show_button.place(relx=0.75, rely=0.24, anchor="sw")
-title_lbl.place(relx=0.03, rely=0.12, anchor="sw")
-enter_pass_lbl.place(relx=0.03, rely=0.24, anchor="sw")
-credit_lbl.place(relx=0.2, rely=0.14, anchor="sw")
-
+app = App(password_checker)
 app.mainloop()
